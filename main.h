120d7244181bf1463e092598c2279b4fb1fed805
valkigman <kvale8@gmail.com>
2022-08-09 18:18:32 -0700

    last try

--- main.h
+++ main.h
@@ -3,22 +3,10 @@
 
 #include <stdarg.h>
 
-/**
- * struct convert - struct for print function
- * @fmt: index
- * @print: pointer for print function
- *
- * Description: stores pointers
- */
-
-typedef struct pt_fmt
-{
-	char *fmt;
-	int (*print)(va_list valist);
-} pt_fmt;
-
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
 
 #endif
--- printf.c
+++ printf.c
@@ -11,16 +11,6 @@ int _printf(const char *format, ...)
 {
 	pt_fmt pt_format[] = {
 		{"c", print_char},
-		{"s", print_string},
-		{"d", print_digit},
-		{"i", print_digit},
-		{"%", print_percent},
-		{"b", print_binary_conv},
-		{"u", print_unsig_int},
-		{"o", print_oct},
-		{"x", print_hex_low},
-		{"X", print_hex_upper},
-		{NULL, NULL}
 	};
 
 	va_list valist;
