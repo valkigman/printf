30d29e222442952b158962fd5998e29ad02a9219
valkigman <kvale8@gmail.com>
2022-08-10 12:39:07 -0700

    no. 1

--- _printf.c
+++ _printf.c
@@ -1,50 +1,90 @@
 #include "main.h"
 
+void cleanup(va_list args, buffer_t *output);
+int run_printf(const char *format, va_list args, buffer_t *output);
+int _printf(const char *format, ...);
+
 /**
- * _printf - produces output according to a format
- * @format: the format string
- * Return: the number of characters printed
+ * cleanup - Peforms cleanup operations for _printf.
+ * @args: A va_list of arguments provided to _printf.
+ * @output: A buffer_t struct.
+ */
+void cleanup(va_list args, buffer_t *output)
+{
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
+}
+
+/**
+ * run_printf - Reads through the format string for _printf.
+ * @format: Character string to print - may contain directives.
+ * @output: A buffer_t struct containing a buffer.
+ * @args: A va_list of arguments.
+ *
+ * Return: The number of characters stored to output.
+ */
+int run_printf(const char *format, va_list args, buffer_t *output)
+{
+	int i, wid, prec, ret = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
+
+	for (i = 0; *(format + i); i++)
+	{
+		len = 0;
+		if (*(format + i) == '%')
+		{
+			tmp = 0;
+			flags = handle_flags(format + i + 1, &tmp);
+			wid = handle_width(args, format + i + tmp + 1, &tmp);
+			prec = handle_precision(args, format + i + tmp + 1,
+					&tmp);
+			len = handle_length(format + i + tmp + 1, &tmp);
+
+			f = handle_specifiers(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret += f(args, output, flags, wid, prec, len);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				ret = -1;
+				break;
+			}
+		}
+		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
+	}
+	cleanup(args, output);
+	return (ret);
+}
+
+/**
+ * _printf - Outputs a formatted string.
+ * @format: Character string to print - may contain directives.
+ *
+ * Return: The number of characters printed.
  */
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	int printed = 0;
+	buffer_t *output;
 	va_list args;
+	int ret;
+
+	if (format == NULL)
+		return (-1);
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
 
 	va_start(args, format);
-	while (format[i])
-	{
-		while (format[i] == '%')
-		{
-			switch (format[i + 1])
-			{
-				case 'c':
-					printed += print_char(args);
-					i += 2;
-					break;
-				case '%':
-					_putchar('%');
-					printed++;
-					i += 2;
-					break;
-				case 's':
-					printed += print_string(args);
-					i += 2;
-					break;
-				default:
-					_putchar(format[i]);
-					_putchar(format[i + 1]);
-					i += 2;
-			}
-		}
-		if (format[i])
-		{
-			_putchar(format[i]);
-			printed++;
-		}
-		i++;
-	}
-	va_end(args);
-	 
-	return (printed);
+
+	ret = run_printf(format, args, output);
+
+	return (ret);
 }
