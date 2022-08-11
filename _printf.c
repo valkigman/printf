1287e0c11d468b5fb3bd5e889ab197c825a02e4a
valkigman <kvale8@gmail.com>
2022-08-11 00:19:30 -0700

    function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
 
 void cleanup(va_list args, buffer_t *output);
 int run_printf(const char *format, va_list args, buffer_t *output);
@@ -56,46 +55,8 @@ int run_printf(const char *format, va_list args, buffer_t *output)
 			{
 				ret = -1;
 				break;
-=======
-/**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
- */
-int _printf(const char * const format, ...)
-{
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-	};
-
-	va_list args;
-	int i = 0, j, len = 0;
-
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-
-Here:
-	while (format[i] != '\0')
-	{
-		j = 13;
-		while (j >= 0)
-		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
->>>>>>> d1ae458c4154413c677bf62e26ab571eb841f37d
 			}
-			j--;
 		}
-<<<<<<< HEAD
 		ret += _memcpy(output, (format + i), 1);
 		i += (len != 0) ? 1 : 0;
 	}
@@ -126,12 +87,4 @@ int _printf(const char *format, ...)
 	ret = run_printf(format, args, output);
 
 	return (ret);
-=======
-		_putchar(format[i]);
-		len++;
-		i++;
-	}
-	va_end(args);
-	return (len);
->>>>>>> d1ae458c4154413c677bf62e26ab571eb841f37d
 }
